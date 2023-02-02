b60d7a587baa569833b38ee5ba7ecd1388a5d8fc
Dean Wagner <dean@dfwagners.net>
2023-02-02 09:30:12 -0500

    REFACTOR: cleaned up text, moved pieces down 0.001

--- src/go/Spaces.jsx
+++ src/go/Spaces.jsx
@@ -1,30 +1,35 @@
-export function Space( {index, color = 'red'})
+import { BlackStone } from './BlackStone.jsx'
+import { WhiteStone } from './WhiteStone.jsx'
+
+function Space( props )
 {
-  const xSpaceSize = 0.17 / 9
-  const xpos = xSpaceSize * ( index % 19 - 9 )
-  const ypos = xSpaceSize * ( Math.floor( index / 19 ) - 9 )
-
-  const turn = 'black'
-
   const turnClick = (e) => {
     console.log('turn', e)
   }
 
-  return <mesh 
-      position={[ xpos, 0.191, ypos]} 
+  return <mesh { ...props }
       rotation-x={ - Math.PI * 0.5 } 
       scale={ 0.015 }
       onClick={ turnClick }
-      userData={{index: index}}
     >
     <planeGeometry />
-    <meshBasicMaterial color={ color } transparent={ true } opacity={ 0.2 } />
+    <meshBasicMaterial color={ 'red' } transparent={ true } opacity={ 0.2 } />
   </mesh>
 }
 
 export function Spaces({ board })
 {  
+  board[200] = 'w'
   return <group>
-    {board.map((params, index) => <Space key={index} index={ index } color={params.color} />)}
+    {board.map((type, index) => {
+        const xSpaceSize = 0.17 / 9
+        const xpos = xSpaceSize * ( index % 19 - 9 )
+        const ypos = xSpaceSize * ( Math.floor( index / 19 ) - 9 )
+        // console.log(index, ypos, xpos)
+        if( type === 'e' ) { return <Space key={index} userData={{index: index}} position={[ xpos, 0.19, ypos ]} /> }
+        if( type === 'b' ) { return <BlackStone key={index} userData={{index: index}} position={[ xpos, 0.19, ypos ]} /> }
+        if( type === 'w' ) { return <WhiteStone key={index} userData={{index: index}} position={[ xpos, 0.19, ypos ]} /> }
+      }
+    )}
   </group>
 }
